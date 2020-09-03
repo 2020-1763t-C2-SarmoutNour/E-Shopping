1d3f57c5a7e6415c375f038c36344f35cb2a8fc4
Christophe Haddad <chriss.hadad@gmail.com>
2020-09-03 20:41:12 +0300

    Add some files and packages for client app

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
+import '../src/styles/index.css';
+
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import 'semantic-ui-css/semantic.min.css';
+import '../src/styles/library/swiper/swiper.css';
+import '../src/styles/library/swiper/swiper.min.css';
+import log from 'loglevel';
+import ErrorBoundary from './ErrorBoundary';
+
+log.disableAll(true);
+
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose();
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div></div>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </Provider>,
   document.getElementById('root'),
 );
