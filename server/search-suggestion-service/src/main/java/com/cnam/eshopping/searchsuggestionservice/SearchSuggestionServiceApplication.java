bcc215eefa76edfec251179ef9837292ff565396
NourElHoudaSarmout <nourelhouda.sarmout@isae.edu.lb>
2020-09-19 23:44:35 +0300

    Update  SearchSuggestionServiceApplication class

--- server/search-suggestion-service/src/main/java/com/cnam/eshopping/searchsuggestionservice/SearchSuggestionServiceApplication.java
+++ server/search-suggestion-service/src/main/java/com/cnam/eshopping/searchsuggestionservice/SearchSuggestionServiceApplication.java
@@ -1,13 +1,19 @@
 package com.cnam.eshopping.searchsuggestionservice;
 
+import com.cnam.eshopping.searchsuggestionservice.controller.SearchSuggestionController;
+import org.json.JSONException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class SearchSuggestionServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SearchSuggestionServiceApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SearchSuggestionServiceApplication.class, args);
+		context.getBean(SearchSuggestionController.class).loadSearchSuggestions();
 	}
 
 }
